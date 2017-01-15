f0ab7cdaf4f9bb160c9651336e2a3a53b22bda47
Henrik Kniberg <h@kniberg.com>
2017-01-15 19:32:27 +0100

    Failing test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
 var chai = require('chai')
 var assert = chai.assert
 
-assert(1 == 1)
+describe('Updater', function() {
+  it('Dumbass test to check if the framework works', function() {
+    assert.equal(1,2);
+  });
+});

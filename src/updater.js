e881ef2889563e409c9fe12ba5fc094575578225
Henrik Kniberg <h@kniberg.com>
2017-01-15 22:27:33 +0100

    If update was needed, it should be downloaded and the snapshot-id file should be updated

--- src/updater.js
+++ src/updater.js
@@ -1,22 +1,51 @@
 const request = require("request")
+const fs = require("fs")
+const path = require('path');
 
 function update(rootDir, updateUrl, callback) {
-  var options = {
+  //Read the deviceId from file
+  const deviceIdFile = path.join(rootDir, "device-id")
+  const deviceId = fs.readFileSync(deviceIdFile)
+
+  //Send the HTTP request
+  const options = {
     uri: updateUrl,
     json: true,
-    method: 'GET'
+    method: 'GET',
+    qs: {
+      'deviceId': deviceId
+    }
   }
   request(options, function(err, response, body) {
+    
+    //Parse the response
     if (err) return callback(err)
     if (body.status === "noUpdateNeeded") {
       callback()
+    } else if (body.status === "updateNeeded") {
+      const snapshotId = getMandatoryResponseProperty(body, "snapshotId")
+      const downloadUrl = getMandatoryResponseProperty(body, "downloadUrl")
+      executeUpdate(rootDir, deviceId, snapshotId, downloadUrl, callback)
     } else {
-      callback(new Error("Unexpected or missing status in response body: ", body))
+      callback(new Error("Unexpected or missing status in response body: " + body))
     }
   })
-
 }
 
+function getMandatoryResponseProperty(body, propertyName) {
+  if (propertyName in body) {
+    return body[propertyName]
+  } else {
+    throw new Error("Can't find property " + propertyName + " in body: " + JSON.stringify(body))
+  }
+}
+
+function executeUpdate(rootDir, deviceId, snapshotId, downloadUrl, callback) {
+  //Create/update the device-id file
+  const snapshotIdFile = path.join(rootDir, "snapshot-id")
+  fs.writeFileSync(snapshotIdFile, snapshotId)
+  callback()
+}
 
 exports.update = update
 
--- test/test.js
+++ test/test.js
@@ -1,55 +1,94 @@
 const chai = require('chai')
 const assert = chai.assert
-const updater = require("../src/updater.js")
-const fs = require('fs')
-const mockFileSystem = require('mock-fs');
-const nock = require('nock'); //http request mocking framework
-const request = require('request')
 
+const fs = require('fs') //File system interaction
+
+const mockFileSystem = require('mock-fs') //Mocks all filesystem access using a fake in-memory fileystem
+const nock = require('nock') //Mocks all http requests
+
+const updater = require("../src/updater.js")
 
 describe('Updater', function() {
+
   before(function() {
     //Create a fake in-memory file system
     mockFileSystem({
-      'home': {
+      '/home': {
         'device-id': 'deviceA'
       }
     })
-    assert.isOk(fs.existsSync("home"))
-    assert.isOk(fs.existsSync("home/device-id"))
+    assert.isOk(fs.existsSync("/home"))
+    assert.isOk(fs.existsSync("/home/device-id"))
   })
 
   after(function() {
+    //Cleanup. Disable the mocks
     mockFileSystem.restore()
     assert.isNotOk(fs.existsSync("home"))
+    nock.cleanAll();
   })
 
   it('If updaterUrl is invalid, update should fail', function(done) {
-    updater.update("/home", 'http://invalid.url', function(err) {
+
+    //Prepare the http mock
+    nock('http://only.this.url.works')
+
+    //Call the updated with a non-existent URL
+    updater.update("/home", 'http://totally.invalid.url', function(err) {
       if (err) {
         done() //Good! The update SHOULD fail!
       } else {
         done(new Error("Hey, the update should have failed!"))
       }
     })
-
   })
 
   it('If no update was needed, then nothing should change', function(done) {
+
+    //Prepare the http mock so it says no update is needed
     nock('http://fakeupdater.com')
       .get("/updateme")
-      .reply(200, {status: "noUpdateNeeded"})
+      .query({
+        'deviceId': 'deviceA'
+      })
+      .reply(200, {
+        'status': 'noUpdateNeeded'
+      })
 
-    //Updater connects to the server and says "Hi, I'm deviceA, and I have snapshot #0."
-    //Server responds "OK" (= nothing to install)
-    //Updater shouldn't change anything.
-
+    //Call the updater
     updater.update("/home", 'http://fakeupdater.com/updateme', function(err) {
       if (err) return done(err)
+
+      //Check that no snapshot-id has been created
       assert.isNotOk(fs.existsSync("home/snapshot-id"))
       done()
     })
   })
 
+  it('If update was needed, it should be downloaded and the snapshot-id file should be updated', function(done) {
+
+    //Prepare the http mock so it says an update is needed
+    nock('http://fakeupdater.com')
+      .get("/updateme")
+      .query({
+        'deviceId': 'deviceA'
+      })
+      .reply(200, {
+        'status': 'updateNeeded',
+        'snapshotId': '1',
+        'downloadUrl': 'http://fakupdate.com/download?deviceId=deviceA&snapshotId=1'
+      })
+
+    //Call the updater
+    updater.update("/home", 'http://fakeupdater.com/updateme', function(err) {
+      if (err) return done(err)
+
+      //Ensure that it created a snapshot-id file
+      assert.isOk(fs.existsSync("/home/snapshot-id"))
+      const snapshotId = fs.readFileSync("/home/snapshot-id")
+      assert.equal(snapshotId, '1')
+      done()
+    })
+  })
 
 })

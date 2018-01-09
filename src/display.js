6d0e7e5b1a6339fe007b9490427e50f14e3ca205
Henrik Kniberg <h@kniberg.com>
2018-01-09 09:50:19 +0100

    If displayRpcPort is left blank in the config, the updater will use console.log instead of trying to communicate with a separate display service. Useful for local testing.

--- src/display.js
+++ src/display.js
@@ -2,26 +2,45 @@ const Promise = require('promise')
 const ProgressBar = require('./progress-bar')
 const callDisplayOverRpc = require('./display-util').callDisplayOverRpc
 
+/**
+ * I know how to display stuff. Updater tells me what's going on, and I make sure it is displayed correctly.
+ * 
+ * If displayRpcPort is given, then I'll use RPC to talk to a separate service that handles display stuff,
+ * using this protocol: https://github.com/sveasmart/display
+ *
+ * If displayRpcPort is not given (or falsy), then I'll just log to the local console.
+ */
 class Display {
   constructor(deviceId, displayRpcPort, mainDisplayTab, networkInfoDisplayTab, logCalls = true) {
     this.deviceId = deviceId
     console.assert(deviceId, "missing deviceId param")
 
     this.displayRpcPort = displayRpcPort
-    console.assert(displayRpcPort, "missing displayRpcPort param")
+    if (this.displayRpcPort) {
+      console.log("I will talk to a display via RPC on port " + this.displayRpcPort)
 
-    this.mainDisplayTab = mainDisplayTab
-    console.assert(mainDisplayTab, "missing mainDisplayTab param")
+      this.mainDisplayTab = mainDisplayTab
+      console.assert(mainDisplayTab, "missing mainDisplayTab param")
 
-    this.networkInfoDisplayTab = networkInfoDisplayTab
-    console.assert(networkInfoDisplayTab, "missing networkInfoDisplayTab param")
+      this.networkInfoDisplayTab = networkInfoDisplayTab
+      console.assert(networkInfoDisplayTab, "missing networkInfoDisplayTab param")
 
-    this.logCalls = logCalls
+      this.logCalls = logCalls
 
-    this.mainTabColumn = 0
+      this.mainTabColumn = 0
+
+    } else {
+      console.log("No displayRpcPort set, so I will use console.log")
+    }
+
   }
 
   showNetworkOk() {
+    if (!this.displayRpcPort) {
+      console.log("Network OK")
+      return
+    }
+
     this._writeLineOnMainTab(7, "Network OK")
 
     this._call("setTexts", [["Network OK"], this.networkInfoDisplayTab ])
@@ -30,6 +49,11 @@ class Display {
   }
 
   showNetworkError(err) {
+    if (!this.displayRpcPort) {
+      console.log("NETWORK ERROR", err)
+      return
+    }
+
     this._writeLineOnMainTab(7, "NETWORK ERROR!")
 
     this._call("setTexts", [["NETWORK ERROR"], this.networkInfoDisplayTab ])
@@ -39,6 +63,11 @@ class Display {
   }
 
   showUpdateError(err) {
+    if (!this.displayRpcPort) {
+      console.log("UPDATE ERROR", err)
+      return
+    }
+
     this._writeLineOnMainTab(7, "UPDATE ERROR!")
 
     this._call("setTexts", [["UPDATE ERROR"], this.networkInfoDisplayTab ])
@@ -48,14 +77,29 @@ class Display {
   }
   
   showUpdatingProgressBar() {
+    if (!this.displayRpcPort) {
+      console.log("Updating...")
+      return
+    }
+
+
     this._writeLineOnMainTab(7, "Updating...")
   }
 
   hideUpdatingProgressBar() {
+    if (!this.displayRpcPort) {
+      console.log("Update done!")
+      return
+    }
     this._writeLineOnMainTab(7, "Update done!")
   }
 
   showDeviceId() {
+    if (!this.displayRpcPort) {
+      console.log("ID: " + this.deviceId.toUpperCase())
+      return
+    }
+
     const deviceIdUpperCase = this.deviceId.toUpperCase()
 
     this._writeLineOnMainTab(6, 'ID: ' + deviceIdUpperCase)
